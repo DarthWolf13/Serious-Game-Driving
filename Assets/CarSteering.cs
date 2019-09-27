2327a4c25e1e220dead1a32035ac6e1bf5fae0a0
Stijn Tromp <stijntromp@chello.nl>
2019-09-27 18:30:22 +0200

    Simple controllable car added as prefab.
    Use the arrow keys to move around with the car.
    Also added the 2D sprite package.

---
+++ Assets/CarSteering.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSteering : MonoBehaviour
+{
+
+    Rigidbody2D rb2d;
+
+    [SerializeField] float AccelerationPower = 5f;
+    [SerializeField] float steeringPower = 5f;
+    float steeringAmount, speed, direction;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        steeringAmount = -Input.GetAxis("Horizontal");
+        speed = Input.GetAxis("Vertical") * AccelerationPower;
+        direction = Mathf.Sign(Vector2.Dot(rb2d.velocity, rb2d.GetRelativeVector(Vector2.up)));
+        rb2d.rotation += steeringAmount * steeringPower * rb2d.velocity.magnitude * direction;
+
+        rb2d.AddRelativeForce(Vector2.up * speed);
+
+        rb2d.AddRelativeForce(-Vector2.right * rb2d.velocity.magnitude * steeringAmount / 2);
+    }
+
+}
