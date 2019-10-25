2c2a9305352bb2300ed364b5ab283234a267c3e9
Stijn Tromp <stijntromp@chello.nl>
2019-10-25 15:41:54 +0200

    Made first part of Question objects and scripts

---
+++ Assets/CarQuestions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarQuestions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "RightAnswer")
+        {
+            //TODO Duidelijk maken dat het antwoord goed was dmv visual/score/geluid
+        }
+
+        if (collision.gameObject.tag == "WrongAnswer")
+        {
+            //TODO Duidelijk maken dat het antwoord fout was dmv visual/score/geluid
+        }
+
+    }
+}
