2b552f26e3d53bece0e6b61bdae0e5a368c30db0
Stijn Tromp <stijntromp@chello.nl>
2019-11-01 13:43:53 +0100

    Game now shows a pop up when you correctly or incorrectly answer a question.
    This is done using UI elements and a coroutine.

--- Assets/Scripts/CarQuestions.cs
+++ Assets/Scripts/CarQuestions.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class CarQuestions : MonoBehaviour
 {
+    public GameObject RightMessage;
+    public GameObject WrongMessage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        RightMessage.SetActive(false);
+        WrongMessage.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,13 +26,28 @@ public class CarQuestions : MonoBehaviour
         {
             //TODO Duidelijk maken dat het antwoord goed was dmv visual/score/geluid
             Debug.Log("Right");
+            StartCoroutine(ShowMessageRight());
         }
 
         if (collision.gameObject.tag == "WrongAnswer")
         {
             //TODO Duidelijk maken dat het antwoord fout was dmv visual/score/geluid
             Debug.Log("Wrong");
+            StartCoroutine(ShowMessageWrong());
         }
+    }
 
+    IEnumerator ShowMessageRight()
+    {
+        RightMessage.SetActive(true);
+        yield return new WaitForSeconds(3);
+        RightMessage.SetActive(false);
+    }
+
+    IEnumerator ShowMessageWrong()
+    {
+        WrongMessage.SetActive(true);
+        yield return new WaitForSeconds(3);
+        WrongMessage.SetActive(false);
     }
 }
